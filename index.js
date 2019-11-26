78ffae69392a0c0af18d8aa1e62f187149549b70
zhouning <zhouning@jinse.com>
2019-11-26 23:07:14 +0800

    feat: compresse multiple files

--- index.js
+++ index.js
@@ -3,46 +3,46 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 const progress = require('./utils/progress')
+const target = path.resolve("target")
 
-const API_KEY = "fvDPnGNpDZRJsrtR5KdM4Qcbp8RvcYhN";
-const target = path.resolve("target");
+const API_KEY = "fvDPnGNpDZRJsrtR5KdM4Qcbp8RvcYhN"
 tinify.key = API_KEY
 
-// 'dir' || 'file'
-let style = process.argv[2]
-// catalog || file
-let detail = process.argv[3]
-
-const MAXLEN = 10
-function fileMin(style, detail) {
-  initProgress(MAXLEN)
-  ValidityApi()
-  switch (style) {
-    case 'file':
-    // 单文件配置
-    compresePic();
-      break;
-    case 'dir':
-      // 目录配置
-      break;
-    default:
-      break;
-  }
+function filePath(fod) {
+  return path.join(__dirname, `./source/`, `${fod}`)
 }
 
-function ValidityApi() {
+const MAXLEN = 15
+let unFinishedFiles = 0
+let finishedFiles = 0
+
+function fileMin() {
+  initProgress(MAXLEN)
+  validityApi()
+    .then(() => {
+      compresePic();
+    })
+    .catch(err => {
+      progress.isDone = true
+      console.log(chalk.bgRed(`ERROR`), chalk.red(err.message))
+    })
+}
+
+function validityApi() {
   // Validation of API key failed.
-  tinify.validate(function (err) {
-    if (err) {
-      console.error(
-        chalk.bgRed(err.status),
-        chalk.bgRed(err.message)
-      );
-    }
-  });
+  return new Promise((resolve, reject) => {
+    tinify.validate(function (err) {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    });
+  })
 }
 
 function initProgress(m) {
+  // init progressing..
+  console.log(chalk.bgBlue.black("INFO"), 'Start compressing...')
   progress.maxLen = m
   let timer = setInterval(function () {
     if (progress.isDone) {
@@ -53,44 +53,75 @@ function initProgress(m) {
   }, 1000)
 }
 
-function compresePic() {
-  // single picture
-  fs.readFile(path.resolve(`./source/${detail}`), (err, file) => {
-    if (err) throw err
-    try {
-      fs.access(target, fs.constants.F_OK, err => {
+function fileReader(fod) {
+  // read file from the source
+  fs.readdir(filePath(fod), (errs, stats) => {
+    unFinishedFiles = stats.length
+    if (errs) throw errs
+    stats.forEach(stat => {
+      fs.stat(filePath(stat), (err, st) => {
         if (err) throw err
-        fs.readdir(target, "", (err, files) => {
-          if (err) throw err
-          if (files.length > 0) {
-            files.map(file => {
-              fs.unlinkSync(`${target}/${file}`, err => {
-                if (err) throw err;
-              });
-            });
-          }
-          fs.rmdir(target, err => {
-            if (err) throw err;
-            fs.mkdir(target, err => {
-              console.log(chalk.bgBlue.black("INFO"), 'Start compressing...')
-              tinify
-                .fromFile(path.resolve(`./source/${detail}`))
-                .toFile(
-                  `${target}/${detail.split(".")[0]}.min.${detail.split(".")[1]}`,
-                  () => {
-                          progress.isDone = true;
-                          console.log(
-                            chalk.bgGreen.black("DONE"),
-                            chalk.green("Successful compression!")
-                          );
-                        }
-                );
-            });
-          });
+        if (st.isFile()) {
+          tinify
+          .fromFile(
+            path.join(__dirname, `/source/${stat}`)
+          )
+          .toFile(`${target}/${stat.split(".")[0]}.min.${stat.split(".")[1]}`, () => {
+            finishedFiles+=1
+            if (finishedFiles == unFinishedFiles) {
+              progress.isDone = true;
+              console.log(
+                chalk.bgGreen.black("DONE"),
+                chalk.green(`${finishedFiles}`),
+                'files were successfully compressed!'
+              );
+            }
+          })
+        } else if (st.isDirectory()) {
+          // console.log(chalk.bgYellow('WARN'), 'Directory is not supported yet..\n')
+          unFinishedFiles -= 1
+        }
+      })
+    })
+  })
+}
+
+function delTarget() {
+  fs.readdir(target, "", (err, files) => {
+    if (err) throw err;
+    if (files.length > 0) {
+      files.map(file => {
+        fs.unlinkSync(`${target}/${file}`, err => {
+          if (err) throw err;
         });
       });
-    } catch (error) {}
+    }
+    fs.rmdir(target, errs => {
+      if (errs) throw errs;
+      fs.mkdir(target, err => {
+        if (err) throw err;
+        fileReader("./");
+      });
+    });
   });
 }
 
-fileMin(style, detail)
+function compresePic() {
+  // comprese image..
+  try {
+    fs.access(target, fs.constants.F_OK, err => {
+      // if there's not a target dir, make it first.
+      if (err) {
+        fs.mkdir(target, () => {
+          delTarget()
+        })
+      } else {
+        delTarget()
+      }
+    });
+  } catch (error) {
+    throw error
+  }
+}
+
+fileMin()
